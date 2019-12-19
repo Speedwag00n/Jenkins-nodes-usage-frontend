5ed0c15d6969d368949f3290585b1d3daf62380c
Ilia Nemankov <ilia.nemankov@gmail.com>
2019-12-19 11:55:40 +0200

    Add chart form title

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="chart-container">
-    <div class="chart-menu">
-      <div class="menu-item" v-for="(item, index) in items" v-bind:key="index">
-        <label class="menu-item-label">{{item.name}}</label>
-        <input type="radio" :value="item.name" v-model="currentNode" v-on:change="onNodeChange">
+    <div class="chart-menu-container">
+      <h1 class="main-header">Select node</h1>
+      <div class="chart-menu">
+        <div class="menu-item" v-for="(item, index) in items" v-bind:key="index">
+          <label class="menu-item-label">{{item.name}}</label>
+          <input type="radio" :value="item.name" v-model="currentNode" v-on:change="onNodeChange">
+        </div>
       </div>
     </div>
     <GChart
@@ -48,5 +51,27 @@ export default {
 <style scoped>
 .empty {
   display: none;
+}
+
+.chart-container {
+  text-align: center;
+}
+
+.chart-menu {
+  display: inline-block;
+  text-align: center;
+  border: 1px solid black;
+  padding: 10px 20px;
+}
+
+.menu-item {
+  display: inline-block;
+  width: 200px;
+  font-size: 22px;
+}
+
+.main-header {
+  font-size: 28px;
+  margin: 10px 0;
 }
 </style>
